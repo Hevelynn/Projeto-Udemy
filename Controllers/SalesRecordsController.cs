194a46cbc1deec7a95be0e378ef75dfe22a013da
Hevelyn Thais <hevelynnthais@gmail.com>
2024-01-05 17:42:53 -0300

    Implementing grouping search

--- Controllers/SalesRecordsController.cs
+++ Controllers/SalesRecordsController.cs
@@ -16,7 +16,7 @@ namespace ProjetoUdemy.Controllers
         {
             return View();
         }
-        public async Task<IActionResult> SimpleSearch(DateTime? minDate, DateTime? maxDate)
+        public async Task<IActionResult> GroupingSearch(DateTime? minDate, DateTime? maxDate)
         {
             if (!minDate.HasValue)
             {
@@ -30,12 +30,26 @@ namespace ProjetoUdemy.Controllers
             ViewData["minDate"] = minDate.Value.ToString("yyyy-MM-dd");
             ViewData["maxDate"] = maxDate.Value.ToString("yyyy-MM-dd");
 
+            var result = await _salesRecordService.FindByDateGroupingAsync(minDate, maxDate);
+            return View(result);
+        }
+
+        public async Task<IActionResult> SimpleSearch(DateTime? minDate, DateTime? maxDate)
+        {
+            if (!minDate.HasValue)
+            {
+                minDate = new DateTime(DateTime.Now.Year, 1, 1);
+            }
+            if (!maxDate.HasValue)
+            {
+                minDate = DateTime.Now;
+            }
+
+            ViewData["minDate"] = minDate.Value.ToString("yyyy-MM-dd");
+            ViewData["maxDate"] = maxDate.Value.ToString("yyyy-MM-dd");
+
             var result = await _salesRecordService.FindByDateAsync(minDate, maxDate);
             return View(result);
-        }        
-        public IActionResult GroupingSearch()
-        {
-            return View();
         }
     }
 }
