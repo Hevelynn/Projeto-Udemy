c2fdeb09831cc13f47075c7a8d7e0c5d5cf305d5
Hevelyn Thais <hevelynnthais@gmail.com>
2024-01-05 17:20:29 -0300

    Implementing simple search

--- obj/Debug/net7.0/ProjetoUdemy.AssemblyInfo.cs
+++ obj/Debug/net7.0/ProjetoUdemy.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjetoUdemy")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9c5c6cdff45e6bc1fb00b3ee577db20f818651c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjetoUdemy")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjetoUdemy")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
